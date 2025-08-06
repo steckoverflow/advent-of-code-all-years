1dadd212632fb4950c9b3025ee040648213d5133
Thomas Steck <thomas@steckoverflow.se>
2025-08-06 21:32:20 +0200

    2015-d1-2

--- 2015/day1.lua
+++ 2015/day1.lua
@@ -17,3 +17,25 @@ print("Part 1:", tbl["("] - tbl[")"])
 -- for k, v in pairs(tbl) do
 -- 	print(k, v)
 -- end
+
+-- Taking different approach will instead add lines to string
+
+local longStr = ""
+
+for line in io.lines("1.txt") do
+	longStr = longStr .. line
+end
+
+local pos = 0
+for i = 1, #longStr do
+	local c = longStr:sub(i, i)
+	if c == "(" then
+		pos = pos + 1
+	elseif c == ")" then
+		pos = pos - 1
+		if pos < 0 then
+			print("Part 2:", i)
+			break
+		end
+	end
+end
