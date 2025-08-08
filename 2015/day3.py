b3c1dae74e4184379e4558561a141aec33188bfc
Thomas Steck <thomas@steckoverflow.se>
2025-08-08 23:13:16 +0200

    2015-d3-2

--- 2015/day3.py
+++ 2015/day3.py
@@ -16,3 +16,31 @@ for m in d:
 
 
 print("Part 1: ", len(set(v)))
+
+v = []
+s = 0, 0
+rs = 0, 0
+for i, m in enumerate(d):
+    if i % 2 == 0:
+        if m == ">":
+            s = (s[0] + 1, s[1])
+        if m == "<":
+            s = (s[0] - 1, s[1])
+        if m == "^":
+            s = (s[0], s[1] + 1)
+        if m == "v":
+            s = (s[0], s[1] - 1)
+        v.append(s)
+    else:
+        if m == ">":
+            rs = (rs[0] + 1, rs[1])
+        if m == "<":
+            rs = (rs[0] - 1, rs[1])
+        if m == "^":
+            rs = (rs[0], rs[1] + 1)
+        if m == "v":
+            rs = (rs[0], rs[1] - 1)
+        v.append(rs)
+
+
+print("Part 2: ", len(set(v)))
