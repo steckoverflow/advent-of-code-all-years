837ba1deb2db4997bac0600fa1ef3a8e88be9bbc
Thomas Steck <thomas@steckoverflow.se>
2025-08-08 23:02:51 +0200

    2015-d3-1

--- 2015/day3.py
+++ 2015/day3.py
@@ -1,7 +1,18 @@
 with open("3.txt", "r") as f:
-    data = f.read().strip()
+    d = f.read().strip()
 
+v = []
 s = 0, 0
+for m in d:
+    if m == ">":
+        s = (s[0] + 1, s[1])
+    if m == "<":
+        s = (s[0] - 1, s[1])
+    if m == "^":
+        s = (s[0], s[1] + 1)
+    if m == "v":
+        s = (s[0], s[1] - 1)
+    v.append(s)
 
 
-print("Part 1: ", len(data))
+print("Part 1: ", len(set(v)))
