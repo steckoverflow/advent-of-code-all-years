ced82d38c030080a5217f4f4f8fd871f6fedc7f8
Thomas Steck <thomas@steckoverflow.se>
2025-08-08 22:41:11 +0200

    2015-d2-2

--- 2015/day2.ts
+++ 2015/day2.ts
@@ -2,12 +2,21 @@ import { readFile } from "node:fs/promises";
 
 const content = await readFile("2.txt");
 const text = content.toString("utf8");
-let total = 0;
+
+let [total, ribbon] = [0, 0];
 for (const line of text.split(/\r?\n/)) {
 	if (!line) continue;
 	const [l, w, h] = line.split("x").map((v) => parseInt(v, 10));
 	const values = [2 * l * w, 2 * w * h, 2 * h * l];
 	total += values.reduce((a, b) => a + b, 0) + Math.min(l * w, w * h, h * l);
+	ribbon +=
+		[l, w, h]
+			.sort((a, b) => a - b)
+			.slice(0, 2)
+			.map((v) => v * 2)
+			.reduce((a, b) => a + b, 0) +
+		l * w * h;
 }
 
 console.log("Part 1: ", total);
+console.log("Part 2: ", ribbon);
