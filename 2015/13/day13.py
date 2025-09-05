625369ab36617a474034022d8456fb739513bd7e
Thomas Steck <thomas@steckoverflow.se>
2025-09-05 09:10:01 +0200

    feat: add day 13 solution for 2015 Advent of Code ```

---
+++ 2015/13/day13.py
@@ -0,0 +1,34 @@
+import pprint
+from dataclasses import dataclass
+
+with open("test.txt", "r") as f:
+    data = f.readlines()
+
+relationships = {}
+for row in data:
+    n, _, c, v, *o, t = row.split(" ")
+    t = t.strip().strip(".")
+    if n not in relationships:
+        relationships[n] = {}
+    if t not in relationships[n]:
+        relationships[n][t] = 0
+    if c == "gain":
+        relationships[n][t] += int(v)
+    else:
+        relationships[n][t] -= int(v)
+    print(n, c, v, t)
+
+for k, v in relationships.items():
+    relationships[k] = sorted(v.items(), key=lambda x: x[1], reverse=True)
+
+sorted_relationships = sorted(
+    relationships.items(), key=lambda x: sum(y[1] for y in x[1]), reverse=True
+)
+
+
+class Person:
+    left: Person
+
+
+pprint.pprint(sorted_relationships)
+print(len(relationships))
